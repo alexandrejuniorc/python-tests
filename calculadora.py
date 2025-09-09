4eff73c9faf48158379d25358fdcf77f486e291d
Alexandre Junior <alexandre.junior@lab2dev.com>
2025-09-08 22:21:10 -0300

    feat: handle with assertions

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,7 @@
 def soma (x, y):
+  # assert serve para validar condições
+  # se a condição for falsa, uma exceção do tipo AssertionError será levantada
+  # Verifica se x e y são números
+  assert isinstance(x, (int, float)), "x deve ser um número"
+  assert isinstance(y, (int, float)), "y deve ser um número"
   return x + y
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from calculadora import soma
 
-print(soma(10, 20))
-print(soma(-10, 20))
-print(soma(1.5, 2.5))
+# print(soma(10, 20))
+# print(soma(-10, 20))
+# print(soma('1.5', 2.5))
+
+try:
+  print(soma('dez', 20))
+except AssertionError as e:
+  print(f'Conta inválida: {e}')
